9bbec5dd604d3af9082887b966dcdac3f030cad9
arcmos <90312162+arcmos@users.noreply.github.com>
2021-11-18 12:43:38 +0100

    Registration script

---
+++ ArcheeRmcRegisterApp.ps1
@@ -0,0 +1,11 @@
+﻿param ([Parameter(Mandatory=$true)]$RedirectUri , [Parameter(Mandatory=$true)]$TenantId, $AppName = "Archee RMC")
+$connectionInfo = Connect-AzureAD -TenantId $TenantId
+
+$reqResAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
+$reqResAccess.ResourceAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList "e1fe6dd8-ba31-4d61-89e7-88639da4683d","Scope"
+$reqResAccess.ResourceAppId = "00000003-0000-0000-c000-000000000000"
+
+$aadApp = New-AzureADApplication -DisplayName $appName -Oauth2AllowImplicitFlow $true -AvailableToOtherTenants $false -ReplyUrls $RedirectUri -RequiredResourceAccess $reqResAccess
+$secret = New-AzureADApplicationPasswordCredential -ObjectId $aadApp.ObjectId -CustomKeyIdentifier "ClientSecret" -EndDate (Get-Date).AddYears(900)
+
+Write-Output "{ `"clientId`": `"$($aadApp.AppId)`", `"clientSecret`": `"$($secret.Value)`", `"userLogin`": `"$($connectionInfo.Account.Id)`" }"
